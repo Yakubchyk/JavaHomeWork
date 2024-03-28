f683fd4e3eb071ce3c98e8d0ac70736a6a0160b2
Yakubchyk <ivan.yakubchyk@gmail.com>
2024-03-28 16:02:14 +0100

    Ver_2 Add class Array_4

--- src/Array_4.java
+++ src/Array_4.java
@@ -1,2 +1,15 @@
+import java.util.Scanner;
+
 public class Array_4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите колличество элементов массива: ");
+
+        int[] arrUser = new int[scanner.nextInt()];
+        for (int i = 0; i < arrUser.length; i++) {
+            arrUser[i] = scanner.nextInt();
+        }
+
+
+    }
 }
