a88b34c1285df40f49f0f8a7cdfd7c068c0f2412
Yakubchyk <ivan.yakubchyk@gmail.com>
2024-03-28 21:53:01 +0100

    Ver_3 Die zweite Aufgabe

--- src/Array_4.java
+++ src/Array_4.java
@@ -9,7 +9,27 @@ public class Array_4 {
         for (int i = 0; i < arrUser.length; i++) {
             arrUser[i] = scanner.nextInt();
         }
-
+        System.out.print("Содердивое массива:   ");
+        for (int ligen : arrUser) {
+            System.out.print(ligen + " ");
+        }
+        /*
+        Задача 1:
+        Пройти по массиву, вывести все элементы в прямом и в обратном порядке.
+         */
+class Array_5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Пользователь сам должен выбрать длинну массива: ");
+        int userMeind = scanner.nextInt();
+        int[] arrRandom = new Math.random() * userMeind;
+    }
+}
 
     }
 }
+
+/*
+Задача 2:
+Найти минимальный-максимальный элементы и вывести в консоль.
+ */
