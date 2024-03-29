43b944ae8cc1a82b25cffe475b3243be9748157c
Yakubchyk <ivan.yakubchyk@gmail.com>
2024-03-29 12:11:48 +0100

    Ver_6 3 Aufgabe wird gemacht

--- src/Array_4.java
+++ src/Array_4.java
@@ -12,6 +12,7 @@ public class Array_4 {
         System.out.print("Введите колличество элементов массива: ");
 // тут пользователь сам ввозит элементы массива
         int[] arrUser = new int[scanner.nextInt()];
+        System.out.println("Заполните массив: ");
         for (int i = 0; i < arrUser.length; i++) {
             arrUser[i] = scanner.nextInt();
         }
@@ -19,6 +20,7 @@ public class Array_4 {
         for (int ligen : arrUser) {
             System.out.print(ligen + " ");
         }
+        // сортировка по возрастанию
         int temp = 0;
         int l = arrUser.length;
         for (int i = 0; i < l; i++) {
@@ -30,9 +32,25 @@ public class Array_4 {
                 }
             }
         }
-//  вывожим отсортированный массив на жкран
+//  вывожим отсортированный массив на äкран
         System.out.println(" ");
         System.out.print("Сортировка по возрастанию: ");
+        for (int i = 0; i < arrUser.length; i++) {
+            System.out.print(arrUser[i] + " ");
+        }
+// сортировка по убыванию
+        for (int i = 0; i < l; i++) {
+            for (int j = i + 1; j < l; j++) {
+                if (arrUser[i] < arrUser[j]) {
+                    temp = arrUser[i];
+                    arrUser[i] = arrUser[j];
+                    arrUser[j] = temp;
+                }
+            }
+        }
+
+        System.out.println(" ");
+        System.out.print("Сортировка по убыванию: ");
         for (int i = 0; i < arrUser.length; i++) {
             System.out.print(arrUser[i] + " ");
         }
@@ -62,23 +80,45 @@ class Array_5 {
             System.out.print(link + " ");
         }
 // сравниваем на предмет нахождения минимума и максимума в массиве
-        // поиск минимума
+        // поиск минимума максимума
         int min = arrRandom[0];
+        int max = arrRandom[0];
+        int minIndex = 0;       // Эти 2 переменные нужны для
+        int maxIndex = 0;       // решения следующей задачи.
+
         for (int i = 1; i < arrRandom.length; i++) {
             if (arrRandom[i] < min) {
                 min = arrRandom[i];
             }
         }
-        System.out.println(" ");
-        System.out.println("Минимальное значение в массиве равно " + min);
-
-        // поиск максимального значение массива
-        int max = arrRandom[0];
         for (int i = 1; i < arrRandom.length; i++) {
             if (arrRandom[i] > max) {
                 max = arrRandom[i];
             }
         }
+        System.out.println(" ");
+        System.out.println("Минимальное значение в массиве равно " + min);
         System.out.println("Максимальное значение в массиве равно " + max);
+
+        /*
+        Задача 3:
+Найти индексы минимального и максимального элементов и вывести в консоль.
+Я выполню эту задачу в рамках упражнения 2.
+//нам нужно найти индексы до того как этот массив будет отсортирован.
+         */
+        for (int i = 1; i < arrRandom.length; i++) {
+            if (arrRandom[i] < arrRandom[minIndex]) {
+                minIndex = i; // Обновляем индекс минимума
+            }
+            if (arrRandom[i] > arrRandom[maxIndex]) {
+                maxIndex = i; // Обновляем индекс максимума
+            }
+        }
+        System.out.println("Индекс максимального значения в массиве: " + maxIndex);
+        System.out.println("Индекс минимального значения в массиве: " + minIndex);
+
     }
 }
+
+
+
